163b99e5bbf0277ca3c8fd05f5214c9e2407e999
aycanurbayindir <e180501003@stud.tau.edu.tr>
2022-06-01 23:25:21 +0300

    login

---
+++ lib/main.dart
@@ -0,0 +1,145 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home:HomePage()
+    );
+  }
+}
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  //Init Firebase HERE
+  Futute<FirebaseApp> _initializeFirebase() async{
+    FirebaseApp firebaseApp  =await Firebase.initializeApp();
+    return FirebaseApp;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+        future: _initializeFirebase(),
+        builder: (context, snapshot){
+          if(snapshot.ConnectionState  == ConnectionState.done ) {
+            return LoginScreen();
+          }
+          return const Center(child: CircularProgressIndicator(),
+
+        },
+      ),
+    );
+  }
+}
+
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  //LoginFunction HERE
+  static Future<User?> loginUsingEmailPassword({required String email, required String password, required BuildContext context) async{
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+    try{
+      UserCredential userCredential  = await auth.signInWithEmailAndPassword(email: email, password: password);
+      user = userCredential.user;
+    }
+    on FirebaseAuthException catch (a){
+      if (a.code == "user-not-found"){
+        print("No user found!");
+      }
+    }
+    return user;
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children:  [
+          const Text("TDU CampusApp",
+            style: TextStyle(
+              color: Colors.cyan,
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Text("Log-In",
+            style: TextStyle(
+              color: Colors.black54,
+              fontSize: 50.0,
+              fontWeight: FontWeight.bold,
+            ),),
+          const SizedBox(
+            height: 44.0,
+          ),
+          const TextField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: "E-mail",
+              prefixIcon: Icon(Icons.mail, color: Colors.black54),
+            ),
+          ),
+          const SizedBox(
+            height: 26.0,
+          ),
+          const TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              hintText: "Passwort",
+              prefixIcon: Icon(Icons.vpn_key_outlined, color:Colors.black54),
+            ),
+          ),
+          const Text("Neu Konto Erstellung",
+            style: TextStyle(color: Colors.cyan),
+          ),
+          const Text("Neu Passwort Erstellung",
+            style: TextStyle(color: Colors.cyan),
+          ),
+          const SizedBox(
+            height: 88.0 ,
+          ),
+          Container(
+            width:  double.infinity,
+            child: RawMaterialButton(
+              fillColor: Colors.cyan,
+              elevation: 0.0,
+              padding: EdgeInsets.symmetric(vertical: 20.0),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)
+              ),
+              onPressed: () {},
+              child: Text("Login", style: TextStyle(color: Colors.black54, fontSize: 18.0, ),),),
+          )
+        ],
+
+      ),
+    );
+  }
+}
